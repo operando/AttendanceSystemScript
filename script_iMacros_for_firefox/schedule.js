fd4952df4fe43ee5d7eb7e3213e98ad0ede2efe3
operando <okanoshinobu8@gmail.com>
2014-08-30 01:00:50 +0900

    Add schedule_holiday Script

--- script_iMacros_for_firefox/schedule.js
+++ script_iMacros_for_firefox/schedule.js
@@ -7,8 +7,10 @@ var loop_date = new Date(dt.getFullYear(), dt.getMonth()+ 1, 0);
 for(var i=0; i < last_day; i++){
     loop_date.setDate(i + 1);
     var day = loop_date.getDay();
+    iimSet("schedule_button", "button_05_schdule_1100170294"+("0"+(i + 1)).slice(-2));
     if(day != 0 && day != 6){
-        iimSet("schedule_button", "button_05_schdule_1100170294"+("0"+(i + 1)).slice(-2));
         iimPlay("schedule");
+    } else {
+        iimPlay("schedule_holiday");
     }
 }
